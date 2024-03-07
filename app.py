a289d7c9084406ad5db0d9f806906cde1c2a6c19
Vridhi Pages <117998089+vridhi-pages@users.noreply.github.com>
2024-03-07 14:48:18 +0000

    done

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def input_pdf_text(uploaded_file):
 
 #Prompt Template
 
-input_prompt = r"""
+input_prompt = """
 Hey Act Like a skilled or very experience ATS(Application Tracking System)
 with a deep understanding of tech field,software engineering,data science ,data analyst
 and big data engineer. Your task is to evaluate the resume based on the given job description.
